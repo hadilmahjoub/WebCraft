fb275a72ddaaa5555a54185bce6612dcc8344c4a
hadil mahjoub <hadil.mahjoub@gmail.com>
2022-08-25 15:07:21 +0200

    project 46 updated | quiz summary added

--- 47 Day 46 - Quiz App/script.js
+++ 47 Day 46 - Quiz App/script.js
@@ -33,24 +33,32 @@ const quizData = [
     },
 ];
 
+const body = document.body;
 const quiz = document.querySelector("#quiz");
-const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.querySelector("#question");
+const answerEls = document.querySelectorAll(".answer");
 const aText = document.querySelector("#a-text");
 const bText = document.querySelector("#b-text");
 const cText = document.querySelector("#c-text");
 const dText = document.querySelector("#d-text");
 const submitBtn = document.querySelector("#submit");
 
+const quizDataAnswers = []; // to save the submitted answers
+const quizDataAnswersClasses = []; // to save for each submited answers a proper class: false or correct
+
 let currentQuiz = 0;
 let score = 0;
 
 loadQuiz();
 
+// submit an answer
 submitBtn.addEventListener("click", () => {
     const answer = getSelected();
 
     if (answer) {
+        // save the submitted answer
+        quizDataAnswers.push(answer);
+
         if (answer === quizData[currentQuiz].correct) {
             score++;
         }
@@ -61,12 +69,17 @@ submitBtn.addEventListener("click", () => {
             loadQuiz();
         } else {
             quiz.innerHTML = `
-                <h2>You answered correctly ar ${score}/${quizData.length} questions</h2>
+                <h2>You answered correctly at ${score}/${quizData.length} questions</h2>
 
-                <button onclick="location.reload()">Reload</button>
+                <div class = "buttons-container">
+                    <button onclick="location.reload()">Reload</button>
+                    <button onclick="loadSummary()">View summary</button>
+                </div>
             `;
         }
     }
+
+    console.log(quizDataAnswers);
 });
 
 function loadQuiz() {
@@ -74,6 +87,7 @@ function loadQuiz() {
 
     const currentQuizData = quizData[currentQuiz];
 
+    // display the next quiz
     questionEl.innerHTML = currentQuizData.question;
     aText.innerHTML = currentQuizData.a;
     bText.innerHTML = currentQuizData.b;
@@ -95,4 +109,72 @@ function getSelected() {
     });
 
     return answer;
+}
+
+function loadSummary() {
+    // clear the document view
+    body.innerHTML = "";
+
+    // create the container for the summary cards
+    const cardsEl = document.createElement("div");
+    cardsEl.classList.add("cards");
+
+    // loop through the data
+    quizData.forEach((quiz, idx) => {
+        // assign each submitted answer its proper class: correct or false
+        if (quiz.correct === quizDataAnswers[idx]) {
+            quizDataAnswersClasses.push("correct");
+        } else {
+            quizDataAnswersClasses.push("false");
+        }
+
+        // create the card element
+        const card = document.createElement("div");
+        card.classList.add("card-container");
+        card.innerHTML = `
+            <div class="card-header">
+                <h2 id="question">${quiz.question}</h2>
+                <ul>
+                    <li><p class = "a">${quiz.a}</p></li>
+                    <li><p class = "b">${quiz.b}</p></li>
+                    <li><p class = "c">${quiz.c}</p></li>
+                    <li><p class = "d">${quiz.d}</p></li>
+                </ul>
+            </div>
+        `;
+
+        // add the card to the container
+        cardsEl.appendChild(card);
+    }); // end foreach (looping through data)
+
+    // reload button
+    const reload = document.createElement("div");
+    reload.classList.add("reload-home");
+    reload.innerHTML = `<button onclick="location.reload()">Reload</button>`;
+
+    // add the container and the button to the body
+    body.appendChild(cardsEl);
+    body.appendChild(reload);
+
+    // get the cards in th summary
+    const cards = document.querySelectorAll(".card-container");
+
+    // loop througn the cards
+    cards.forEach((card, i) => {
+        // get the listed answers for each question
+        const ps = card.querySelectorAll("p");
+
+        // loop througn the answers to hightight the correct answer and test with the submitted answer
+        ps.forEach((p) => {
+            // find the correct answer (from the data)
+            if (p.className === quizData[i].correct) {
+                p.parentNode.classList.add("correct");
+            }
+
+            // assign the submitted answer its proper class
+            if (p.className === quizDataAnswers[i]) {
+                p.parentNode.classList.add(quizDataAnswersClasses[i]);
+            }
+        });
+    }); // end foreach (looping through summary cards)
 }
